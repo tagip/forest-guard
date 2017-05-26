875218bde37d95a5276e61ddcacb44afecc1461c
Mika Andrianarijaona <mika@tag-ip.com>
2017-05-26 16:11:30 +0300

    fix: display ref instead of id

--- src/TaskField.js
+++ src/TaskField.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Chip from 'material-ui/Chip';
 
-const TaskField = ({ record = {} }) => <Chip style={{margin: 2}}><span title={record.subject}>#{record.id}: {record.subject}</span></Chip>;
+const TaskField = ({ record = {} }) => <Chip style={{margin: 2}}><span title={record.subject}>#{record.ref}: {record.subject}</span></Chip>;
 
 export default TaskField;
