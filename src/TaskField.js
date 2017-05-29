70e7571fd63511894e594d4278b8d7bcf92d1b89
Mika Andrianarijaona <mika@tag-ip.com>
2017-05-29 16:25:26 +0300

    fix: security issue without rel="noopener noreferrer"

--- src/TaskField.js
+++ src/TaskField.js
@@ -4,7 +4,7 @@ import { BASE_URL } from './consts';
 
 const TaskField = ({ record = {}, type = 'task' }) =>
   <Chip style={{margin: 2}}>
-    <a href={`${BASE_URL}/project/${record.project_extra_info.slug}/${type}/${record.ref}`} target="_blank">
+    <a href={`${BASE_URL}/project/${record.project_extra_info.slug}/${type}/${record.ref}`} target="_blank" rel="noopener noreferrer">
       <span title={record.subject}>#{record.ref}: {record.subject}</span>
     </a>
   </Chip>;
