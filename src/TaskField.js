a5e92a5ee0ca243cd3a7ad2ae953e07f036a6d46
Mika Andrianarijaona <mika@tag-ip.com>
2017-05-29 16:41:00 +0300

    style: add a nice avatar with status color if available

--- src/TaskField.js
+++ src/TaskField.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Chip from 'material-ui/Chip';
+import Avatar from 'material-ui/Avatar';
 import { BASE_URL } from './consts';
 
-const TaskField = ({ record = {}, type = 'task' }) =>
-  <Chip style={{margin: 2}}>
-    <a href={`${BASE_URL}/project/${record.project_extra_info.slug}/${type}/${record.ref}`} target="_blank" rel="noopener noreferrer">
-      <span title={record.subject}>#{record.ref}: {record.subject}</span>
-    </a>
-  </Chip>;
+const TaskField = function ({ record = {}, type = 'task' }) {
+  const backgroundColor = record.status_extra_info.color || '#e0e0e0';
+  return (
+    <Chip style={{margin: 2}}>
+      <Avatar backgroundColor={backgroundColor}>
+      </Avatar>
+      <a
+        style={{color: '#0e0e0e', textDecoration: 'none'}}
+        href={`${BASE_URL}/project/${record.project_extra_info.slug}/${type}/${record.ref}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span title={record.subject}>#{record.ref}:{record.subject}</span>
+      </a>
+    </Chip>);
+}
 
 export default TaskField;
