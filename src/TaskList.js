cf21a3d60d09f31cd699839ede0b1bb33d795d66
Mika Andrianarijaona <mika@tag-ip.com>
2017-05-30 15:04:15 +0300

    feat: display project name

--- src/TaskList.js
+++ src/TaskList.js
@@ -5,6 +5,7 @@ export const TaskList = (props) => (
   <List {...props}>
     <Datagrid>
       <TextField source="id" />
+      <TextField source="project_extra_info.name" label="Project" />
       <TextField source="subject"/>
       <TextField source="status_extra_info.name" label="Status"/>
       <TextField source="assigned_to_extra_info.full_name_display" label="Assignee"/>
