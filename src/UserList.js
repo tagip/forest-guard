904a61a7a2b60a98c1cc1c38160a6f39b716fbee
Mika Andrianarijaona <mika@tag-ip.com>
2017-05-31 09:57:46 +0300

    chore: change some wording

--- src/UserList.js
+++ src/UserList.js
@@ -4,20 +4,20 @@ import { List, Datagrid, TextField, ReferenceManyField, SingleFieldList} from 'a
 import TaskField from './TaskField';
 
 export const UserList = (props) => (
-    <List {...props} perPage={30} title="Who is doing what?">
-        <Datagrid>
-            <TextField source="id" sortable={false}/>
-            <TextField source="full_name" sortable={false}/>
-            <ReferenceManyField label="Tasks" reference="tasks" target="assigned_to" filter={{status__is_closed:false}}>
-                <SingleFieldList>
-                    <TaskField source="id" type="task" />
-                </SingleFieldList>
-            </ReferenceManyField>
-            <ReferenceManyField label="Issues" reference="issues" target="assigned_to" filter={{status__is_closed:false}}>
-                <SingleFieldList>
-                    <TaskField source="id" type="issue" />
-                </SingleFieldList>
-            </ReferenceManyField>
-        </Datagrid>
-    </List>
+  <List {...props} perPage={30} title="Work In Progress">
+      <Datagrid>
+          <TextField source="id" sortable={false}/>
+          <TextField source="full_name" sortable={false}/>
+          <ReferenceManyField label="Tasks" reference="tasks" target="assigned_to" filter={{status__is_closed:false}}>
+              <SingleFieldList>
+                  <TaskField source="id" type="task" />
+              </SingleFieldList>
+          </ReferenceManyField>
+          <ReferenceManyField label="Issues" reference="issues" target="assigned_to" filter={{status__is_closed:false}}>
+              <SingleFieldList>
+                  <TaskField source="id" type="issue" />
+              </SingleFieldList>
+          </ReferenceManyField>
+      </Datagrid>
+  </List>
 );
