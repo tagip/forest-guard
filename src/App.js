3e08ac3ef0eb980ecbfb3ca33064d298a89477cd
Mika Andrianarijaona <mika@tag-ip.com>
2017-06-13 11:29:37 +0300

    feat: enable french translation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 // in src/App.js
 import React from 'react';
-import { Admin, Resource } from 'admin-on-rest';
+import { Admin, Resource, englishMessages, resolveBrowserLocale } from 'admin-on-rest';
+import frenchMessages from 'aor-language-french';
 import authClient from './authClient';
 import { UserList } from './UserList';
 import { restClient } from './RestClient';
 
+const messages = {
+  fr: frenchMessages,
+  en: englishMessages,
+};
+
 const App = () => (
-    <Admin title="Forest Guard" restClient={restClient} authClient={authClient}>
-        <Resource name="users" list={UserList} options={{ label: 'Work In Progress' }}/>
-        <Resource name="tasks" />
-        <Resource name="issues" />
-        <Resource name="project" />
-    </Admin>
+  <Admin title="Forest Guard" restClient={restClient} authClient={authClient} locale={resolveBrowserLocale()} messages={messages}>
+    <Resource name="users" list={UserList} options={{ label: 'Work In Progress' }} />
+    <Resource name="tasks" />
+    <Resource name="issues" />
+    <Resource name="project" />
+  </Admin>
 );
 
 export default App;
