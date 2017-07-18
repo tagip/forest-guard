49d58f265ed7e5b78761eb88e2f6007854d8ea2d
Mika Andrianarijaona <mikaoelitiana@users.noreply.github.com>
2017-07-18 14:05:27 +0300

    fix due date parsing

--- src/consts.js
+++ src/consts.js
@@ -4,7 +4,7 @@ export const BASE_URL = process.env.REACT_APP_BASE_URL || 'https://tree.taiga.io
 let dueDates = {};
 if (process.env.REACT_APP_DUE_DATE_CUSTOM_ATTRIBUTES) {
   try {
-    dueDates = JSON.parse(JSON.parse("\"" + process.env.REACT_APP_DUE_DATE_CUSTOM_ATTRIBUTES + "\""));
+    dueDates = JSON.parse(process.env.REACT_APP_DUE_DATE_CUSTOM_ATTRIBUTES);
   } catch (e) {
     console.error('Due date parameter is wrong', e);
   }
