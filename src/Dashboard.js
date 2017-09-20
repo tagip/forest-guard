435fc78459d3c52ec3a40e0f05bbf5fd09cc31d4
Mika Andrianarijaona <mika@tag-ip.com>
2017-09-20 11:59:04 +0300

    get user projects

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { AppBarMobile, GET_LIST, GET_MANY } from 'admin-on-rest';
+import withWidth from 'material-ui/utils/withWidth';
 import { Card, CardHeader, CardText, CardTitle } from 'material-ui/Card';
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
-} from 'material-ui/Table';
+import { restClient } from './RestClient';
+import MyProjects from './MyProjects';
 
-export default () => (
-    <div>
-      <Card style={{ margin: '2em' }}>
-          <CardTitle title="My projects" />
-          <CardText>Lorem ipsum sic dolor amet...</CardText>
-      </Card>
-    </div>
-);
+class Dashboard extends Component {
+  state = {};
+
+  componentDidMount() {
+    const user_id = localStorage.getItem('user_id');
+    restClient(GET_LIST, 'projects', {
+      filter: { member: user_id },
+      pagination: { page: 1, perPage: 50 },
+    })
+    .then(response => {
+      this.setState({projects: response.data})
+    })
+  }
+
+  render(){
+    const { projects } = this.state;
+
+    return (
+      <div>
+        <MyProjects projects={projects} />
+      </div>
+  );
+  }
+}
+
+export default withWidth()(Dashboard);
---
+++ src/MyProjects.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Card, CardTitle } from 'material-ui/Card';
+import { List, ListItem } from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+import { translate } from 'admin-on-rest';
+import { pink400 } from 'material-ui/styles/colors';
+import { BASE_URL } from './consts';
+
+const style = { flex: 1 };
+
+export default translate(({ projects = [], translate }) => (
+    <Card style={style}>
+        <CardTitle title={translate('My Projects')} />
+        <List>
+            {projects.map(record =>
+                <ListItem
+                    key={record.id}
+                    href={`${BASE_URL}/project/${record.slug}`}
+                    target="blank"
+                    primaryText={record.name}
+                    secondaryText={record.description}
+                    leftAvatar={<Avatar backgroundColor={pink400}>{record.name.charAt(0)}</Avatar>}
+                />
+            )}
+        </List>
+    </Card>
+));
