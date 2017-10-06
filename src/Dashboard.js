2201ea5143792251f976ad55373a9b4a54f0b4e6
Mika Andrianarijaona <mika@tag-ip.com>
2017-10-06 15:20:34 +0300

    fix: array merge issues

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { GET_LIST } from 'admin-on-rest';
 import withWidth from 'material-ui/utils/withWidth';
-import { map, extend, merge } from 'lodash';
+import { map, extend, concat } from 'lodash';
 import { restClient } from './RestClient';
 import MyProjects from './components/MyProjects';
 import ItemList from './components/ItemList';
@@ -20,30 +20,6 @@ class Dashboard extends Component {
       this.setState({projects: response.data})
     })
 
-    restClient(GET_LIST, 'issues', {
-      filter: {
-        assigned_to: user_id,
-        status__is_closed: false
-      },
-      pagination: { page: 1, perPage: 50 },
-    })
-    .then(response => {
-      const issues = map(response.data, issue => extend(issue, {type: "issue"}))
-      this.setState({issues: merge(this.state.issues, issues)})
-    })
-
-    restClient(GET_LIST, 'issues', {
-      filter: {
-        assigned_to: "null",
-        status__is_closed: false
-      },
-      pagination: { page: 1, perPage: 50 },
-    })
-    .then(response => {
-      const issues = map(response.data, issue => extend(issue, {type: "issue"}))
-      this.setState({unassigned_issues: merge(this.state.unassigned_issues, issues)})
-    })
-
     restClient(GET_LIST, 'tasks', {
       filter: {
         assigned_to: user_id,
@@ -53,7 +29,7 @@ class Dashboard extends Component {
     })
     .then(response => {
       const tasks = map(response.data, task => extend(task, {type: "task"}))
-      this.setState({tasks: merge(this.state.tasks, tasks)})
+      this.setState({tasks: concat(this.state.tasks, tasks)})
     })
 
     restClient(GET_LIST, 'tasks', {
@@ -65,7 +41,31 @@ class Dashboard extends Component {
     })
     .then(response => {
       const tasks = map(response.data, issue => extend(issue, {type: "task"}))
-      this.setState({unassigned_issues: merge(this.state.unassigned_issues, tasks)})
+      this.setState({unassigned_issues: concat(this.state.unassigned_issues, tasks)})
+    })
+
+    restClient(GET_LIST, 'issues', {
+      filter: {
+        assigned_to: user_id,
+        status__is_closed: false
+      },
+      pagination: { page: 1, perPage: 50 },
+    })
+    .then(response => {
+      const issues = map(response.data, issue => extend(issue, {type: "issue"}))
+      this.setState({issues: concat(this.state.issues, issues)})
+    })
+
+    restClient(GET_LIST, 'issues', {
+      filter: {
+        assigned_to: "null",
+        status__is_closed: false
+      },
+      pagination: { page: 1, perPage: 50 },
+    })
+    .then(response => {
+      const issues = map(response.data, issue => extend(issue, {type: "issue"}))
+      this.setState({unassigned_issues: concat(this.state.unassigned_issues, issues)})
     })
 
   }
